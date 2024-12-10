3c9de80a48413646fe740e9a8924cc34c5937b54
pfhgil <99049610+pfhgil@users.noreply.github.com>
2024-12-10 03:57:59 +0300

    enum_reflect

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ struct AggregateStruct
 {
     int a = 0;
     const float m_member = 4;
+    std::string b = "";
 };
 
 struct AggregateStruct2
@@ -62,6 +63,47 @@ public:
     };
 };
 
+constexpr bool strings_equal(char const * a, char const * b) {
+    return std::string_view(a)==b;
+}
+
+consteval bool str(std::string_view a, std::string_view b)
+{
+    if(a == b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+enum class MyEnum
+{
+    VALUE0,
+    VALUE1,
+    VALUE2,
+    VALUE3
+};
+
+template<typename T>
+struct ABC
+{
+    constexpr ABC(T t)
+    {
+        val = t;
+    }
+
+    virtual void d()
+    {
+
+    }
+
+// private:
+    T val;
+};
+
 int main()
 {
     static constexpr AggregateStruct s3 { };
@@ -70,6 +112,49 @@ int main()
     constexpr auto meta = makeMetaInfo(s3);
     // runtime meta
     auto runtimeMeta = meta.asRuntime();
+
+    meta.iterateThroughMembers([](auto memberInfo) {
+        std::cout << memberInfo.unmangled_name << std::endl;
+    });
+
+    if constexpr(std::remove_reference_t<decltype(makeMetaInfo<const AggregateStruct>(AggregateStruct { }).template get<2>())>::unmangled_name == "b")
+    {
+        std::cout << "AAAABBBB" << std::endl;
+    }
+
+    /*if constexpr(getUnMangledValueName<enum_reflect::getEnumMembers<MyEnum>()[3]>() == "MyEnum::VALUE2")
+    {
+        std::cout << "AAAABBBBcccccc" << std::endl;
+    }*/
+
+    constexpr auto enumMeta = enum_reflect::getEnumMembersInfo<MyEnum>();
+
+    enum_reflect::iterateThroughEnumMembers<MyEnum>([](auto memberInfo) {
+        std::cout << memberInfo.unmangled_name << std::endl;
+    });
+
+    std::cout << "Runtime name: " << enum_reflect::generateRuntimeNamesMarkup<MyEnum>()[2] << std::endl;
+
+    /*enum_reflect::iterateThroughEnumMembers<MyEnum>([](auto memberValue) {
+        using t = std::underlying_type_t<decltype(memberValue)>;
+        // std::cout << static_cast<t>(memberValue) << std::endl;
+        if constexpr(memberValue == MyEnum::VALUE0)
+        {
+
+        }
+    });*/
+
+    // std::cout << getEnumMembersCount<MyEnum>() << std::endl;
+
+    //std::cout << enumSize<MyEnum>() << std::endl;
+    // std::cout << getEnumMembersCount<MyEnum>() << std::endl;
+    // std::cout << getEnumMembersCount<MyEnum>() << std::endl;
+
+    // std::cout << std::remove_reference_t<decltype(makeMetaInfo<const AggregateStruct>(AggregateStruct { }).template get<0>())>::unmangled_name << std::endl;
+
+    // static_assert(str("abc", "abc"), "strings are equal");
+
+    // auto& mem = meta.getByName("a");
 
     // runtimeMeta.findMember("m_megaMember")->setValue<std::string>("changed by runtime reflection");
 
